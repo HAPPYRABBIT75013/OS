53bf7d3ea893efcde8cbcb3ea54a0c3763088d36
xvqinghe <bingtangxiaoshui@Outlook.com>
2022-12-31 11:51:19 +0800

    更新 0.0.1.py
    add more “os”

--- 0.0.1.py
+++ 0.0.1.py
@@ -9,13 +9,15 @@ while True:
     if input_code == 'help':
         print("欢迎使用本窗口，没想到吧，此OS支持（仅支持)中文呦""但由于python的问题，所以暂且还是需要输入英文指令~~~")
         pygame.time.delay(100)
-        print("about:关于此OS" "  ""help：显示此帮助" "  " "exit：退出此OS" "  " "open:打开文件" "math:打开计算器")
+        print("about:关于此OS" "  ""help：显示此帮助" "  " "exit：退出此OS" "  " "open:打开文件" "math:打开计算器" "" "(\请用双斜杠代替)")
     elif input_code == "exit":
+        print("再见")
+        pygame.time.delay(100)
         exit()
     elif input_code == "about":
         print("版本号：0.0.0")
     elif input_code == "open":
-        path = input("请输入文件路径及文件名(\请用双斜杠代替)")
+        path = input("请输入文件路径及文件名")
         os.system(path)  # path为文件路径，本目录下可直接写文件名os 为本地模块不需再行安装
     elif input_code == "math":
         check_math = print(os.system('C:\\Windows\\System32\\calc.exe'))
@@ -23,3 +25,8 @@ while True:
     elif input_code == "command":
         command = input("请输入指令")
         os.system(command)
+        print("已执行")
+    elif input_code == "remove":
+        remove = input("删除文件路径及名称")
+        os.remove(remove)
+        print("已执行")
